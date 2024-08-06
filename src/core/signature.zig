0de274cf0d8b8430b45c31049ae2704b0e42bc93
Harold Newman <hnewman2744@gmail.com>
2024-08-07 02:48:03 +1000

    Implement base 58 encode and decode methods for signature

--- src/core/signature.zig
+++ src/core/signature.zig
@@ -1,10 +1,15 @@
 const std = @import("std");
 const Pubkey = @import("pubkey.zig").Pubkey;
+const base58 = @import("base58-zig");
 const Ed25519 = std.crypto.sign.Ed25519;
 const Verifier = std.crypto.sign.Ed25519.Verifier;
 const e = std.crypto.errors;
 
+const BASE_58_ENCODER = base58.Encoder.init(.{});
+const BASE_58_DECODER = base58.Decoder.init(.{});
+
 pub const SIGNATURE_LENGTH: usize = 64;
+pub const SIGNATURE_BASE58_LENGTH: usize = 88;
 
 pub const Signature = struct {
     data: [SIGNATURE_LENGTH]u8 = [_]u8{0} ** SIGNATURE_LENGTH,
@@ -33,5 +38,24 @@ pub const Signature = struct {
 
     pub fn eql(self: *const Self, other: *const Self) bool {
         return std.mem.eql(u8, self.data[0..], other.data[0..]);
+    }
+
+    pub fn toBase58EncodedString(self: *const Signature) error{EncodingError}![SIGNATURE_BASE58_LENGTH]u8 {
+        var dest: [SIGNATURE_BASE58_LENGTH]u8 = undefined;
+        @memset(&dest, 0);
+        const written = BASE_58_ENCODER.encode(&self.data, &dest) catch return error.EncodingError;
+        if (written > SIGNATURE_BASE58_LENGTH) {
+            std.debug.panic("written is > {}, written: {}, dest: {any}, bytes: {any}", .{ SIGNATURE_BASE58_LENGTH, written, dest, self.data });
+        }
+        return dest;
+    }
+
+    pub fn fromBase58EncodedString(encoded: []const u8) error{DecodingError}!Self {
+        var dest: [SIGNATURE_LENGTH]u8 = undefined;
+        const written = BASE_58_DECODER.decode(encoded, &dest) catch return error.DecodingError;
+        if (written != SIGNATURE_LENGTH) {
+            return error.DecodingError;
+        }
+        return Self.init(dest);
     }
 };
