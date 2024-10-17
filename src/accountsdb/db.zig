b8a73bae2edba479bf2e8592767b25b27a7231ef
x19 <100000306+0xNineteen@users.noreply.github.com>
2024-10-17 17:03:28 -0400

    fixes

--- src/accountsdb/db.zig
+++ src/accountsdb/db.zig
@@ -3987,7 +3987,7 @@ pub const BenchmarkAccountsDBSnapshotLoad = struct {
 
 pub const BenchmarkAccountsDB = struct {
     pub const min_iterations = 1;
-    pub const max_iterations = 200;
+    pub const max_iterations = 5;
 
     pub const MemoryType = enum {
         ram,
@@ -4098,7 +4098,7 @@ pub const BenchmarkAccountsDB = struct {
         //     .accounts = .disk,
         //     .n_accounts_multiple = 2, // 1 mill accounts init
         //     .index = .ram,
-        //     .name = "3M accounts (3_slot - ram index - disk accounts)",
+        //     .name = "3M accounts (3_slot - ram index - disk accounts - 1million init)",
         // },
 
         // // testing disk indexes
